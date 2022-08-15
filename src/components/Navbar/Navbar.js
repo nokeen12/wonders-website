9bd79e772c691532eb71da95a5728c7e7e2935e5
Nokeen <cordrepayne@gmail.com>
2022-08-14 22:28:41 -0500

    Started navbar links for desktop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,19 @@
 import './Navbar.css'
 import WonderLogo from "../Logo/WonderLogo";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <div className='navbar'>
-            <WonderLogo />
-            
-            <HamburgerMenu />
+            <div className="leftSide">
+                <WonderLogo />
+            </div>
+            <div className="rightSide">
+                <Link to="/home">Home</Link>
+                <Link to="/book-now">Booking</Link>
+                <Link to="/about">About</Link>
+                <HamburgerMenu />
+            </div>
         </div>
     )
 }
