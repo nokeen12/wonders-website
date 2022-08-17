54b03ddcd193c899f05b815e1618667c36ab2da3
Nokeen <cordrepayne@gmail.com>
2022-08-16 23:58:40 -0500

    added sending email for booking

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -3,7 +3,23 @@ function Booking(){
     return(
         <div>
             <Link to="/">Back to homepage</Link>
-            booking page
+            <br/>
+            <p>Booking Page</p>
+            <form action="mailto:cordrepayne@gmail.com" method="get" enctype="text/plain">
+                <label for="name">Name:
+                    <input type="text" name="name" id="name" />
+                </label>
+                <br/>
+                <label for="email">Email:
+                    <input type="text" name="email" id="email" />
+                </label>
+                <br/>
+                <label for="date">Date:
+                    <input type="date" name="date" id="date"/>
+                </label>
+                <br/>
+                <input type="submit" name="submit" value="Book"/>
+            </form>
         </div>
     )
 }
