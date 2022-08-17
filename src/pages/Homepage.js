7b690d54b3f3683f4a211c8b53b348258b6f953a
Nokeen <cordrepayne@gmail.com>
2022-08-17 17:35:08 -0500

    removed banner media query(not needed), add homepage media query and styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,14 +5,15 @@ function Homepage(){
     return(
         <div className="homepage">
             <Banner/>
-            <div id="section2">
-                <div className="section2Child"><WonderLogo/></div>
-                <div className="section2Child"><CallToAction/></div>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et ex ut diam interdum feugiat et at metus. Vestibulum tincidunt fermentum facilisis. Aenean bibendum auctor ex ac consequat. Donec quis orci non libero aliquet pellentesque. Vivamus non commodo urna. Integer aliquam eros vel nisi elementum, et cursus eros efficitur. Aliquam bibendum velit ultricies lorem facilisis tincidunt. Nullam consectetur, diam non aliquam accumsan, eros orci condimentum sapien, non consequat tortor justo fringilla ligula. Nullam luctus finibus quam. Nulla aliquam leo ac laoreet finibus. Sed sapien ex, sodales ac elit eu, laoreet rhoncus augue. Praesent convallis iaculis eros in semper. Integer vitae lobortis urna. Suspendisse commodo at arcu vel eleifend.
-                </p>
-                
+            <div id="bodyContent">
+                <div id="leftBody">
+                    <div id="leftBodyLogo"><WonderLogo/></div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et ex ut diam interdum feugiat et at metus. Vestibulum tincidunt fermentum facilisis. Aenean bibendum auctor ex ac consequat. Donec quis orci non libero aliquet pellentesque. Vivamus non commodo urna. Integer aliquam eros vel nisi elementum, et cursus eros efficitur. Aliquam bibendum velit ultricies lorem facilisis tincidunt. Nullam consectetur, diam non aliquam accumsan, eros orci condimentum sapien, non consequat tortor justo fringilla ligula. Nullam luctus finibus quam. Nulla aliquam leo ac laoreet finibus. Sed sapien ex, sodales ac elit eu, laoreet rhoncus augue. Praesent convallis iaculis eros in semper. Integer vitae lobortis urna. Suspendisse commodo at arcu vel eleifend.</p>
+                </div>
+                <div id="rightBody">
+                    <div id="rightBodyButton"><CallToAction/></div> 
+                    {/* insert image carrousel component here  */}
+                </div>
             </div>
         </div>
     )
